b9be80ff9df18903195880913f83a8192a41f526
Rahul Vaghela <twelvesprings.rahul.vaghela@gmail.com>
2023-02-21 14:49:25 +0530

    With mongo Configuration

--- src/main/java/com/employee/Entity/Employee.java
+++ src/main/java/com/employee/Entity/Employee.java
@@ -3,27 +3,21 @@ package com.employee.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.persistence.*;
+
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity
-@Table(name = "employeedata")
+@Document(value = "employeeInformation")
 public class Employee {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
-    @Column(name = "first_name",nullable = false)
     private String firstName;
-
-    @Column(name = "last_name",nullable = false)
     private String lastName;
-
-    @Column(name = "email",nullable = false,unique = true)
     private String email;
 
 
--- src/main/java/com/employee/Repository/EmployeeRepository.java
+++ src/main/java/com/employee/Repository/EmployeeRepository.java
@@ -2,9 +2,9 @@ package com.employee.Repository;
 
 
 import com.employee.Entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+public interface EmployeeRepository extends MongoRepository<Employee,Long> {
 }
